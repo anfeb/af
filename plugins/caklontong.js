dc0fb94788a7f1a1a8f640d24a386548a1d1e237
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-20 02:34:08 +0700

    Update caklontong.js

--- plugins/caklontong.js
+++ plugins/caklontong.js
@@ -20,10 +20,10 @@ Ketik ${usedPrefix}calo untuk bantuan
 Bonus: ${poin} XP
 `.trim()
     conn.caklontong[id] = [
-        await conn.send2Button(m.chat, caption.trim(), '© stikerin', 'BANTUAN', '.calo', 'NYERAH', 'nyerah'),
+        await conn.send2Button(m.chat, caption.trim(), '© @ANFEBN', 'BANTUAN', '.calo', 'NYERAH', 'nyerah'),
         json, poin,
         setTimeout(async () => {
-            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.keterangan}`, '© stikerin', 'CAK LONTONG', '.caklontong')
+            if (conn.caklontong[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*\n${json.keterangan}`, '© @ANFEBN', 'CAK LONTONG', '.caklontong')
             delete conn.caklontong[id]
         }, timeout)
     ]

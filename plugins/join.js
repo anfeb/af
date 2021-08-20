189a65cfbcbc351f11936f64afb5cf41cbc0bca8
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-20 23:29:43 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -16,7 +16,7 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 ketik *${usedPrefix}menu* untuk melihat daftar perintah`.trim(), '© stikerin', 'MENU', `${usedPrefix}?`, { contextInfo: { mentionedJid: [m.sender] } })
 }
 handler.help = ['join <chat.whatsapp.com>']
-handler.tags = ['']
+handler.tags = ['Premium']
 
 handler.command = /^join$/i
 

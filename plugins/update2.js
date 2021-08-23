6a15d42d41e73353cc470dda94444694d47a6379
anfeb <88554978+anfeb@users.noreply.github.com>
2021-08-23 23:24:12 +0700

    Update update2.js

--- plugins/update2.js
+++ plugins/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'anfeb/afr'
+let repository = 'anfeb/af'
 let branch = 'main'
 
 async function handler(m, { text }) {
